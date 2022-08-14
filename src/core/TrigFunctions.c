108cebd2097b6194672a650bc71f0dd9654b5ac7
origamiscienceguy <taylorsimsscience@gmail.com>
2022-08-13 23:48:09 -0500

    Trig functions
    I would like to take the trig tables out of the sphere function and make them their own separate files.
    
    For now, you can leave them inside the sphere function, and I'll delete them whenever I rework that function. In the meantime, I've created some trig functions so you can use the tables without having to deal with the strange interlacing.

---
+++ src/core/TrigFunctions.c
@@ -0,0 +1,128 @@
+#include "seven/base/types.h"
+#include "TrigFunctions.h"
+
+//All angular inputs and outputs are in units of pi/512 radians.
+//All linear inputs and outputs are in units of 1/256.
+
+s32 GetSinValue(s32 theta){
+	u32 negative;
+	s32 value;
+	theta = theta & 0x3ff; //shift any Theta into a range of 0-1023 (1 complete sin wave)
+	if(theta > 0x200){ //if theta is in the negative portion of the sin wave
+		negative = 1;
+		theta -= 0x200;
+	}
+	if(theta > 0x100){ //if theta is in the second or fourth quarter of the sin wave.
+		theta = 200 - theta;
+	}
+	if(theta < 0x100){
+		value = table_group_2[theta] & 0xff;
+	}
+	else{
+		value = 0x100;
+	}
+	if (negative){
+		value = -value;
+	}
+	return value;
+}
+
+s32 GetCosValue(s32 theta){
+	s32 value;
+	value = GetSinValue(theta + 0x100);
+	return value;
+}
+
+s32 GetArcSinValue(s32 length){ //length must be between -256 (-1) and 256 (1)
+	s32 theta;
+	u32 negative;
+	if (length < 0){
+		negative = 1;
+		length = -length;
+	}
+	if (length != 0x100){
+		theta = table_group_1[length] & 0xff;
+	}
+	else {
+		theta = 0x100;
+	}
+	if(negative){
+		theta = -theta;
+	}
+	return theta;
+}
+
+s32 GetArcCosValue(s32 length){ //length must be between -0x100 (-1) and 0x100 (1)
+	s32 theta;
+	theta = 0x100 - GetArcSinValue(length);
+	return theta;
+}
+
+s32 GetPythSqrtValue(s32 length){ //length must be between -0x100 (-1) and 0x100 (1)
+	u32 value;
+	if (length < 0){
+		length = -length;
+	}
+	if (length != 0x100){
+		value = (table_group_1[length] & 0xff00) >> 8;
+	}
+	else{
+		value = 0;
+	}
+	return value;
+}
+
+s32 GetInvPythSqrtValue(s32 length){ //length must be between -0xff and 0xff. 0x100 is infinity, but returns 0xffff
+	u32 value;
+	if (length < 0){
+		length = -length;
+	}
+	if (length != 0x100){
+		value = (table_group_1[length] & 0xffff0000) >> 16;
+	}
+	else{
+		value = 0xffff;
+	}
+	return value;
+}
+
+s32 GetCscValue(s32 theta){ //length should not be a multiple of pi (512). csc(pi) is undefined, but this function returns 0xffff
+	u32 negative;
+	s32 value;
+	theta = theta & 0x3ff; //shift any Theta into a range of 0-1023 (1 complete csc wave)
+	if(theta > 0x200){ //if theta is in the negative portion of the csc wave
+		negative = 1;
+		theta -= 0x200;
+	}
+	if(theta > 0x100){ //if theta is in the second or fourth quarter of the csc wave.
+		theta = 200 - theta;
+	}
+	if(theta < 0x100){
+		value = (table_group_2[theta] & 0xffff0000) >> 16;
+	}
+	else{
+		value = 0x100;
+	}
+	if (negative){
+		value = -value;
+	}
+	return value;
+}
+
+s32 GetSecValue(s32 theta){ //length should not be a multiple of pi (512) - pi/2. csc(pi/2) is undefined, but this function returns 0xffff
+	s32 value;
+	value = GetCscValue(theta + 0x100);
+	return value;
+}
+
+s32 GetTanValue(s32 theta){ //theta should not be a multiple of pi (256) - pi/2. Answer is undefined, but this function returns 0xffff
+	s32 value;
+	value = (GetSecValue(theta) * GetSinValue(theta)) >> 8;
+	return value;
+}
+
+s32 GetCotValue(s32 theta){ //theta should not be a multiple of pi (256). Answer is undefined, but this function returns 0xffff
+	s32 value;
+	value = (GetCscValue(theta) * GetCosValue(theta)) >> 8;
+	return value;
+}
